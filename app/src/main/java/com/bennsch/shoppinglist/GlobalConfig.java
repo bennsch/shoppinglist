5f3b5a495e1b47d906ab7a828e8a5643f8c312be
bennsch <megabob5000@gmx.de>
2024-09-22 10:17:38 -0400

    Debug settings added

--- app/src/main/java/com/bennsch/shoppinglist/GlobalConfig.java
+++ app/src/main/java/com/bennsch/shoppinglist/GlobalConfig.java
@@ -1,8 +1,7 @@
 package com.bennsch.shoppinglist;
 
 public class GlobalConfig {
-    public static final Boolean DBG_SHOW_INCIDENCE              = false;
-    public static final Boolean DBG_DYNAMIC_COLOR_ENABLED       = true;
-    public static final Boolean DBG_SHOW_NAVDRAWER_ACTIONVIEW   = false;
-    public static final int     DBG_DYNAMIC_COLOR_SEED          = 0xFAD058;
+    public static Boolean DBG_SHOW_INCIDENCE              = true;
+    public static Boolean DBG_SHOW_NAVDRAWER_ACTIONVIEW   = false;
+    public static int     DBG_DYNAMIC_COLOR_SEED          = 0xFAD058;
 }
--- app/src/main/java/com/bennsch/shoppinglist/MyApplication.java
+++ app/src/main/java/com/bennsch/shoppinglist/MyApplication.java
@@ -2,7 +2,11 @@ package com.bennsch.shoppinglist;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.SharedPreferences;
 import android.util.Log;
+
+import androidx.preference.PreferenceManager;
+
 import com.bennsch.shoppinglist.data.PreferencesRepository;
 import com.google.android.material.color.DynamicColors;
 import com.google.android.material.color.DynamicColorsOptions;
@@ -16,6 +20,12 @@ public class MyApplication extends Application {
     public void onCreate() {
         Log.d(TAG, "onCreate: ");
         super.onCreate();
+
+        // TODO: Remove this debug code
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        GlobalConfig.DBG_SHOW_INCIDENCE = preferences.getBoolean("dbg_show_incidence", false);
+        GlobalConfig.DBG_SHOW_NAVDRAWER_ACTIONVIEW = preferences.getBoolean("dbg_show_navdrawer_actionview", false);
+
 
         PreferencesRepository preferencesRepository = PreferencesRepository.getInstance(this);
         Boolean useDynamicColors = preferencesRepository.getPrefUseDynamicColors().getValue();
