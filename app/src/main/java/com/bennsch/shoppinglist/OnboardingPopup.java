dc3d272ef661eb8b0889ccfd3a426e63f21cddc4
bennsch <megabob5000@gmx.de>
2025-06-21 08:21:27 -0400

    OnboardingPopup: Use better text animation

--- app/src/main/java/com/bennsch/shoppinglist/OnboardingPopup.java
+++ app/src/main/java/com/bennsch/shoppinglist/OnboardingPopup.java
@@ -3,8 +3,8 @@ package com.bennsch.shoppinglist;
 import android.content.Context;
 import android.view.Gravity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -13,34 +13,30 @@ import com.google.android.material.snackbar.Snackbar;
 public class OnboardingPopup {
     // Wrapper for a popup.  Using a Snackbar is just one possible implementation
     private final Snackbar mSnackbar;
+    private final TextView mTextView;
+    private final int mAnimDuration;
 
     public OnboardingPopup(Context context, View root) {
+        // Get animation duration from context.
+        mAnimDuration = context.getResources().getInteger(android.R.integer.config_shortAnimTime);
         mSnackbar= Snackbar.make(root, "", Snackbar.LENGTH_INDEFINITE);
-        // mSnackbar.setAnchorView(mBinding.fab);
-        // mSnackbar.setAnchorViewLayoutListenerEnabled(true);
         mSnackbar.setAction("Dismiss", v -> {});
         mSnackbar.setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE);
         mSnackbar.setBackgroundTint(
                 ThemeHelper.getColor(
                         com.google.android.material.R.attr.colorTertiary, context));
-        TextView textView = mSnackbar.getView().findViewById(
+        mTextView = mSnackbar.getView().findViewById(
                 com.google.android.material.R.id.snackbar_text);
-        textView.setMinLines(3);
-        textView.setMaxLines(3);
-        textView.setGravity(Gravity.CENTER);
-        textView.setPadding(
-                ThemeHelper.DpToPx(10, context), 0,
-                ThemeHelper.DpToPx(10, context), 0);
-        // textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_about, 0, 0,0);
-        // textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
-        // textView.setEms(100);
+        mTextView.setMinLines(3);
+        mTextView.setMaxLines(3);
+        mTextView.setGravity(Gravity.CENTER);
     }
 
     public void show(String text) {
-        mSnackbar.setText(text);
         if (mSnackbar.isShown()) {
-            flashText();
+            setTextAnimated(mTextView, text, mAnimDuration);
         } else {
+            mTextView.setText(text);
             mSnackbar.show();
         }
     }
@@ -49,15 +45,23 @@ public class OnboardingPopup {
         mSnackbar.dismiss();
     }
 
-    private void flashText() {
-        TextView view = mSnackbar.getView().findViewById(com.google.android.material.R.id.snackbar_text);
-        Animation fadeOut = new ScaleAnimation(
-                1.0f, 1.1f, 1.0f, 1.1f,
-                Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF, 0.5f);
-        fadeOut.setDuration(250);
-        fadeOut.setRepeatMode(Animation.REVERSE);
-        fadeOut.setRepeatCount(1);
-        view.startAnimation(fadeOut);
+    private void setTextAnimated(TextView textView, String text, int animDur) {
+        Animation anim = new AlphaAnimation(1.0f, 0.0f);
+        anim.setDuration(animDur);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(1);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {}
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                textView.setText(text);
+            }
+        });
+        textView.startAnimation(anim);
     }
 }
