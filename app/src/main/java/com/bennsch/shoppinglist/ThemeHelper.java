5c32347eb6924561ae1ae81fcb97e9d7e6848c38
bennsch <bennsch.dev@gmail.com>
2025-12-21 08:56:04 -0500

    Using custom colorError for SDK < 26

--- app/src/main/java/com/bennsch/shoppinglist/ThemeHelper.java
+++ app/src/main/java/com/bennsch/shoppinglist/ThemeHelper.java
@@ -1,6 +1,8 @@
 package com.bennsch.shoppinglist;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.util.TypedValue;
 
 import androidx.annotation.AnyRes;
@@ -21,9 +23,14 @@ public class ThemeHelper {
     }
 
     public static @ColorInt int getColor(int resId, @NonNull Context context) {
-        TypedValue typedValue = new TypedValue();
-        context.getTheme().resolveAttribute(resId, typedValue, true);
-        return ContextCompat.getColor(context, typedValue.resourceId);
+        if ((Build.VERSION.SDK_INT < 26) && (resId == android.R.attr.colorError)) {
+            // android.R.attr.colorError was introduced in SDK 26
+            return Color.rgb(186, 26, 26);
+        } else {
+            TypedValue typedValue = new TypedValue();
+            context.getTheme().resolveAttribute(resId, typedValue, true);
+            return ContextCompat.getColor(context, typedValue.resourceId);
+        }
     }
 
     public static int dpToTx(int dp, Context context) {
